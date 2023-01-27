2566a420e49105ba3f5ee691a9921282eda1eda9
wang shu xie <wangshu@ualberta.ca>
2023-01-27 12:46:13 -0700

    commit

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.listycity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener {
+
+    private ArrayList<City> dataList;
+    private ListView cityList;
+    private CityArrayAdapter cityAdapter;
+
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] cities = {
+                "Edmonton", "Vancouver", "Toronto"
+        };
+        String[] provinces = {
+                "AB", "BC", "ON"
+        };
+
+        dataList = new ArrayList<>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        cityList = findViewById(R.id.city_list);
+        cityAdapter = new CityArrayAdapter(this, dataList);
+        cityList.setAdapter(cityAdapter);
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int item_clicked, long l) {
+                System.out.println(item_clicked);
+                dataList.remove(item_clicked);
+                new AddCityFragment().show(getSupportFragmentManager(), "edit city");
+
+                //dataList.add(item_clicked,);
+            }
+
+
+        });
+
+        //dataList.set(0,dataList.get(dataList.size()-1));
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> {
+            new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+        });
+    }
+
+}
